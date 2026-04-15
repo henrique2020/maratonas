fed7c175bda2178354477ae80b2e122456abeb62
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-04-15 15:28:39 -0300

    Ho Ho Ho

--- Java/1759.java
+++ Java/1759.java
@@ -1,5 +1,5 @@
 // Problema: 1759 - Ho Ho Ho | Resposta: Accepted
-// Linguagem: Java 19 [+2s]  | Tempo: 0.066s
+// Linguagem: Java 19 [+2s]  | Tempo: 0.062s
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,15 +7,12 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int rep = Integer.parseInt(br.readLine());
+        int rep = Integer.parseInt(
+            new BufferedReader(
+                new InputStreamReader(System.in)
+            ).readLine()
+        );
 
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i < rep; i++) { sb.append("Ho "); }
-        sb.append("Ho!");
-
-        System.out.println(sb.toString());
-        
-        br.close();
+        System.out.println("Ho ".repeat(rep - 1) + "Ho!");
     }
 }
