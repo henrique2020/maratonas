f0bffae3a10dbac042c7e9d7a18caf7caa1f3288
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-04-20 14:30:14 -0300

    Números Pares

--- JavaScript/1059.js
+++ JavaScript/1059.js
@@ -1,4 +1,9 @@
-let i;
-for(i = 1; i <= 50; i++){
-    console.log(i*2);
-}
+// Problema: 1059 - Números Pares    | Resposta: Accepted
+// Linguagem: JavaScript 12.18 [+2s] | Tempo: 0.034s
+
+const [inicio, fim] = [1, 100];
+const pares = [...Array(fim - inicio + 1).keys()]
+    .map(i => i + inicio)
+    .filter(n => n % 2 === 0);
+    
+console.log(pares.join('\n'));
