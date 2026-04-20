f7847f8ff7cf3b4e732300678c23834c08d11c1b
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-04-20 14:28:57 -0300

    Números Pares

--- PHP/1059.php
+++ PHP/1059.php
@@ -1,5 +1,9 @@
 <?php
-for($i = 1; $i <= 50; $i++){
-	printf("%d\n", $i*2);
-}
+// Problema: 1059 - Números Pares | Resposta: Accepted
+// Linguagem: PHP (8.1.2) [+1s]   | Tempo: 0.000s
+
+$EOL = PHP_EOL;
+[$inicio, $fim] = [1, 100];
+$pares = array_filter(range($inicio, $fim), fn($n) => $n % 2 == 0);
+echo implode("{$EOL}", $pares) . $EOL;
 ?>
