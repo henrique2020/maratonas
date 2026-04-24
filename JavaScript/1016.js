83e25f4a6de3fe9d0626389d8f525b0a63506ffa
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-04-24 14:44:09 -0300

    Distância

---
+++ JavaScript/1016.js
@@ -0,0 +1,5 @@
+// Problema: 1016 - Distância        | Resposta: Accepted
+// Linguagem: JavaScript 12.18 [+2s] | Tempo: 0.023s
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+console.log(`${parseInt(input.split('\n')) * 2} minutos`);
