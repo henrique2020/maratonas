637e1c3c68f6bf9a3a7ff1921d132209804faa02
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-04-24 09:55:26 -0300

    Extremamente Básico

---
+++ C++/1001.cpp
@@ -0,0 +1,16 @@
+// Problema: 1001 - Extremamente Básico | Resposta: Accepted
+// Linguagem: C20++ [+0s]               | Tempo: 0.000s
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int A, B, X;
+    
+    cin >> A >> B;
+    X = A + B;
+    cout << "X = " << X << endl;
+    
+    return 0;
+}
