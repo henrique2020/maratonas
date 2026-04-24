41d885e6212cac04f9fde4a4a3a6be72cc01cf8e
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-04-24 09:59:38 -0300

    Hello World!

---
+++ C++/1000.cpp
@@ -0,0 +1,10 @@
+// Problema: 1000 - Hello World! | Resposta: Accepted
+// Linguagem: C20++ [+0s]        | Tempo: 0.000s
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
