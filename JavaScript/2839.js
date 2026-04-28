a45ab3d90900018971c9f342e4959c8fbd4f7832
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-04-28 14:38:11 -0300

    As Meias de Rangel

---
+++ JavaScript/2839.js
@@ -0,0 +1,5 @@
+// Problema: 2839 - As Meias de Rangel | Resposta: Accepted
+// Linguagem: JavaScript 12.18 [+2s]   | Tempo: 0.063s
+
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+console.log(parseInt(input) + 1);
