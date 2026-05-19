7ca74476ebc5744ed30b4e9e170aaf3d11da836e
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-05-18 21:46:20 -0300

    Diferença

---
+++ C++/1007.cpp
@@ -0,0 +1,14 @@
+// Problema: 1007 - Diferença | Resposta: Accepted
+// Linguagem: C20++ [+0s]     | Tempo: 0.000s
+
+#include <iostream>
+
+using namespace std;
+int main() {
+    int a, b, c, d;
+
+    cin >> a >> b >> c >> d;
+    cout << "DIFERENCA = " << (a * b) - (c * d) << endl;
+
+    return 0;
+}
