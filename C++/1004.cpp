a29a92f43819ef65dbfaecb4b19ad58e8e235040
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-05-18 21:35:18 -0300

    Produto Simples

---
+++ C++/1004.cpp
@@ -0,0 +1,14 @@
+// Problema: 1004 - Produto Simples | Resposta: Accepted
+// Linguagem: C20++ [+0s]           | Tempo: 0.000s
+
+#include <iostream>
+
+using namespace std;
+int main() {
+    int n1, n2;
+
+    std::cin >> n1 >> n2;
+    cout << "PROD = " << n1 * n2 << endl;
+
+    return 0;
+}
