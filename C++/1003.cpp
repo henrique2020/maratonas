a191efe52548b264a5294372b2d8885d131c243b
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-05-18 21:25:27 -0300

    Soma Simples

---
+++ C++/1003.cpp
@@ -0,0 +1,14 @@
+// Problema: 1003 - Soma Simples | Resposta: Accepted
+// Linguagem: C20++ [+0s]        | Tempo: 0.000s
+
+#include <iostream>
+
+using namespace std;
+int main() {
+    int n1, n2;
+
+    std::cin >> n1 >> n2;
+    cout << "SOMA = " << n1 + n2 << endl;
+
+    return 0;
+}
