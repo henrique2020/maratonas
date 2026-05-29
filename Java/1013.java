51db39d5f5c2b6c46ee3016949b425e94e90f36e
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-05-28 21:26:31 -0300

    O Maior

--- Java/1013.java
+++ Java/1013.java
@@ -1,33 +1,30 @@
 // Problema: 1013 - O Maior | Resposta: Accepted
-// Linguagem: Java 19 [+2s] | Tempo: 0.302s
+// Linguagem: Java 19 [+2s] | Tempo: 0.096s
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	
-	public static int calculo(int n1, int n2) {
-		return (n1 + n2 + Math.abs(n1-n2));
-	}
+
+    public static int maior(int a, int b) {
+        return (a + b + Math.abs(a - b)) / 2;
+    }
 
     public static void main(String[] args) throws IOException {
-    	InputStreamReader ir = new InputStreamReader(System.in);
-	    BufferedReader in = new BufferedReader(ir);
-	    
-	    String[] linha = in.readLine().split(" ");
-		int 
-			a = Integer.parseInt(linha[0]),
-			b = Integer.parseInt(linha[1]),
-			c = Integer.parseInt(linha[2]),
-			maior;
-		
-		maior = (int) (calculo(a, b) / 2);
-		maior = (int) (calculo(maior, c) / 2);
-		
-		System.out.printf("%d eh o maior\n", maior);
+
+        BufferedReader in =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        String[] linha = in.readLine().split(" ");
+        int a = Integer.parseInt(linha[0]);
+        int b = Integer.parseInt(linha[1]);
+        int c = Integer.parseInt(linha[2]);
+
+        int maior = maior(maior(a, b), c);
+
+        System.out.printf("%d eh o maior%n", maior);
         
         in.close();
-        ir.close();
     }
 }
