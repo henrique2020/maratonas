0d55d993e7161437db74f7a8df1318119ffde581
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-05-31 00:00:06 -0300

    Busca na Internet

---
+++ C++/2413.cpp
@@ -0,0 +1,12 @@
+// Problema: 2413 - Busca na Internet | Resposta: Accepted
+// Linguagem: C20++ [+0s]             | Tempo: 0.000s
+
+#include <iostream>
+
+int main() {
+    int clicks;
+    std::cin >> clicks;
+    std::cout << clicks * 4 << std::endl;
+
+    return 0;
+}
