d37560e41acd795b725590919818aeca1fca82cf
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-06-01 20:15:40 -0300

    Macacos da Torre de Hanoi

--- Python/2681.py
+++ Python/2681.py
@@ -1,8 +1,17 @@
-x = int(input())
-s = int(pow(2,x)-1)
-h = int(s**0.036)
-h= h%24
-m = (s%3600)/60
-s = (s%3600)%60
+# Problema: 2681 - Macacos da Torre de Hanoi | Resposta: Accepted
+# Linguagem: Python 3.11 [+1s]               | Tempo: 0.000s
+
+import sys
+MOD = 86400 #Segundos em um dia
+
+entrada = sys.stdin.read().split()
+for val in entrada:
+    n = int(val)
+    s = (pow(2, n, MOD) - 1) % MOD
     
-print('%.2d:%.2d:%.2d'%(h,m,s))
+    horas = s // 3600
+    resto = s % 3600
+    minutos = resto // 60
+    segundos = resto % 60
+    
+    print(f'{horas:02d}:{minutos:02d}:{segundos:02d}')
