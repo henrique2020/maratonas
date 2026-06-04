96f134408718a3e2d6a7cbc732f844ff076a15b2
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-06-04 19:28:39 -0300

    Figurinhas

--- PHP/1028.php
+++ PHP/1028.php
@@ -1,15 +1,19 @@
 <?php
-// Problema: 1028 - Figurinhas  | Resposta: Runtime error
-// Linguagem: PHP (8.1.2) [+1s] | Tempo: 0.178s
+// Problema: 1028 - Figurinhas  | Resposta: Accepted
+// Linguagem: PHP (8.1.2) [+1s] | Tempo: 0.203s
+
+function gcd(int $a, int $b): int {
+    if ($b == 0) { return $a; }
+    return gcd($b, $a % $b);
+}
 
 $casos = intval(trim(fgets(STDIN)));
-
 for ($i = 0; $i < $casos; $i++) {
     $entrada = array_map(
         'intval', 
         explode(' ', trim(fgets(STDIN)))
     );
 
-    echo (int) gmp_gcd($entrada[0], $entrada[1]) . PHP_EOL;
+    echo gcd($entrada[0], $entrada[1]) . PHP_EOL;
 }
 ?>
