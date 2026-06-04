d80450f58a481632aee4b87cd89044e809f0f8d8
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-06-04 19:35:31 -0300

    Problema no Elevador

--- Python/3250.py
+++ Python/3250.py
@@ -1,33 +1,30 @@
-# Problema: 3250 - Problema no Elevador | Resposta: Time limit exceeded
-# Linguagem: Python 3.11 [+1s]          | Tempo: 2.000s
+# Problema: 3250 - Problema no Elevador | Resposta: Accepted
+# Linguagem: Python 3.11 [+1s]          | Tempo: 0.403s
 
 from collections import deque
 
-def movimenta(a, i, o, s, d):
-    visitados = [False for _ in range(a+1)]
-    fila = deque([(i, 0)])
+def movimenta(andares, inicio, objetivo, sobe, desce):
+    visitados = [False] * (andares + 1)
+    visitados[inicio] = True
     
+    fila = deque([(inicio, 0)]) 
     while fila:
         andar, movimentos = fila.popleft()
-        if andar == o:
+        if andar == objetivo:
             return movimentos
         
-        movimentos += 1
-        sobe = andar + s
-        desce = andar - d
+        prox_sobe = andar + sobe
+        if prox_sobe <= andares and not visitados[prox_sobe]:
+            visitados[prox_sobe] = True 
+            fila.append((prox_sobe, movimentos + 1))
         
-        if sobe <= andares and not visitados[sobe]:
-            fila.append((sobe, movimentos))
-            visitados[andar] = True
-            
-        if desce >= 1 and not visitados[desce]:
-            fila.append((desce, movimentos))
-            visitados[andar] = True
+        prox_desce = andar - desce
+        if prox_desce >= 1 and not visitados[prox_desce]:
+            visitados[prox_desce] = True
+            fila.append((prox_desce, movimentos + 1))
     
     return -1
 
 andares, inicio, objetivo, sobe, desce = map(int, input().split())
 movimentos = movimenta(andares, inicio, objetivo, sobe, desce)
-
-if movimentos != -1: print(movimentos)
-else: print("use the stairs")
+print(movimentos if movimentos != -1 else "use the stairs")
