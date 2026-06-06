706c123600d7cbacfb3b35887d7d9fa79cbf4c4b
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-06-06 10:41:16 -0300

    Troco

--- Python/2446.py
+++ Python/2446.py
@@ -1,16 +1,19 @@
-# Problema: 2446 - Troco       | Resposta: Time limit exceeded
-# Linguagem: Python 3.11 [+1s] | Tempo: 2.000s
+# Problema: 2446 - Troco       | Resposta: Accepted
+# Linguagem: Python 3.11 [+1s] | Tempo: 0.022s
 
-valor = int(input().split()[0])
-moedas = map(int, input().split())
+import sys
 
-dp = [False] * (valor + 1)
-dp[0] = True 
+entrada = sys.stdin.read().split()    
+valor = int(entrada[0])
 
-for moeda in moedas:
-    for i in range(valor, moeda - 1, -1):
-        if dp[i - moeda]: dp[i] = True
-            
-    if dp[valor]: break
+dp = 1
+bit_alvo = 1 << valor
+possivel = False
+for i in range(2, len(entrada)):
+    moeda = int(entrada[i])
+    dp |= (dp << moeda)
+    if dp & bit_alvo:
+        possivel = True
+        break
 
-print("S" if dp[valor] else "N")
+print("S" if possivel else 'N')   
