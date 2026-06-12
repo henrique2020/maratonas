0f0a7507588b9ea9d68f5ee6c42782b26e08bed1
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-06-11 21:53:24 -0300

    Consulta e Alteração

--- Python/3306.py
+++ Python/3306.py
@@ -1,16 +1,35 @@
-from math import gcd
+# Problema: 3306 - Consulta e Alteração | Resposta: Accepted
+# Linguagem: Python 3.11 [+1s]          | Tempo: 0.051s
 
-param, queries = map(int, input().split())
-lista = list(map(int, input().split()))
-for x in range(queries):
-    try:
-        dados = list(map(int, input().split()))
-        posIni = dados[1]-1
-        posFim = dados[2]
-        if dados[0] == 1:
-            for x in range(posIni, posFim):
-                lista[x] += dados[3]
-        else:
-            print(gcd(*lista[posIni:posFim]))
-    except:
-        print(x+1)
+import sys
+from math import gcd
+from functools import reduce
+
+entrada = sys.stdin.read().split()
+ 
+param = int(entrada[0])
+queries = int(entrada[1])
+
+lista = [int(x) for x in entrada[2:param+2]]
+
+it = iter(entrada[param+2:])
+saida = []
+for _ in range(queries):
+    tipo = int(next(it))
+    
+    if tipo == 1:
+        posIni = int(next(it)) - 1
+        posFim = int(next(it))
+        v = int(next(it))
+        
+        for i in range(posIni, posFim):
+            lista[i] += v
+            
+    else:
+        posIni = int(next(it)) - 1
+        posFim = int(next(it))
+        
+        resultado = reduce(gcd, lista[posIni:posFim])
+        saida.append(str(resultado))
+            
+sys.stdout.write('\n'.join(saida) + '\n')
