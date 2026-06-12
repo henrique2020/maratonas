14b4cf9178fbf9cdaaecf09f3fd69480a630b6eb
Henrique <48599271+henrique2020@users.noreply.github.com>
2026-06-11 21:33:48 -0300

    Circuito Bioquímico Digital

--- Python/1486.py
+++ Python/1486.py
@@ -1,10 +1,23 @@
+# Problema: 1486 - Circuito Bioquímico Digital | Resposta: Accepted
+# Linguagem: Python 3.11 [+1s]                 | Tempo: 0.324s
+
 while True:
     p, n, c = map(int, input().split())
     if(p == 0 and n == 0 and c == 0): break
     
-    entradas = 0
+    sequencia = [0] * p
+    palitos_validos = 0
     for _ in range(n):
-        comprimento = sum(map(int, input().split()))
-        if(comprimento >= c): entradas += 1
+        valores = list(map(int, input().split()))
+        for i, v in enumerate(valores):               
+                if v == 1: sequencia[i] += 1
+                else:
+                    if sequencia[i] >= c: 
+                        palitos_validos += 1
+                    sequencia[i] = 0
     
-    print(entradas)
+    for seq in sequencia:
+        if seq >= c:
+            palitos_validos += 1
+            
+    print(palitos_validos)
